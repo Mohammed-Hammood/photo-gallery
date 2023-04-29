5fb7f2b39ed15c6579badfd3e4dfe4930c3527c3
Mohammed <iraqiProfessionalDesigner@gmail.com>
2023-04-30 01:19:10 +0700

    updates

--- src/pages/images.tsx
+++ src/pages/images.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import { Filters, ImageMagnifier, Loader, Modal, Pagination } from "components";
 import { useAppDispatch, useAppSelector } from "store/hooks";
 import { setImages, setImagesFilters } from "store/slicers/images";
@@ -8,7 +8,7 @@ import { Endpoints } from "libs";
 import { ImagesWrapper } from "styles/images";
 import { Helmet } from "react-helmet-async";
 import { useTranslation } from "react-i18next";
-import { selectGlobal, selectImages } from "store/selectors";
+import { selectGlobal, selectImages, selectImagesByFilters } from "store/selectors";
 import { setGlobalParams } from "store/slicers/global";
 
 
@@ -21,26 +21,20 @@ interface CallbackProps {
 
 export default function ImagesPage() {
     const { images, images_count, filters } = useAppSelector(selectImages);
+    const queryset = useAppSelector(selectImagesByFilters)
     const url = Endpoints.images(filters);
     const { t } = useTranslation("translation");
     const [activeImage, setActiveImage] = useState<ImageTypes | null>(null);
+
     const dispatch = useAppDispatch();
-    const {limit, page } = filters;
+    const { limit, page } = filters;
     const max = limit * page;
-    const min = (page - 1) * limit; 
+    const min = (page - 1) * limit;
     const { filters: { direction }, search: SeachModal } = useAppSelector(selectGlobal)
     const callback = ({ data: { images_count, images } }: CallbackProps): void => {
         dispatch(setImages({ images_count: images_count, images: images }));
     }
-    const { setUrl, loading } = useFetch({ callback, url: images_count === 0 ? url : null });
-    const filtersRef = useRef(filters)
-
-    useEffect(() => {
-        if (JSON.stringify(filters) !== JSON.stringify(filtersRef.current)) {
-            // filtersRef.current = filters;
-            // setUrl(url);
-        }
-    }, [filters, images, filtersRef, url, setUrl]);
+    const { loading } = useFetch({ callback, url: images_count === 0 ? url : null });
 
     if (loading) return <Loader size={100} position="fixed" />
     return (<>
@@ -57,15 +51,15 @@ export default function ImagesPage() {
             <div className="center-content">
                 {images_count > filters.limit ?
                     <Pagination
-                        queryset={images}
-                        totalCount={images_count}
+                        queryset={queryset}
+                        totalCount={queryset.length}
                         setPage={(page: number) => dispatch(setImagesFilters({ key: 'page', value: page }))}
                         page={filters.page}
                         limit={filters.limit}
                     />
                     : null}
                 <div className="images__wrapper">
-                    {images && images.filter(item => item.allowed_users === 'all').filter((item, index:number)=> index < max && index >= min).map((image, index: number) => {
+                    {queryset.filter((item, index: number) => index < max && index >= min).map((image, index: number) => {
                         return (
                             <div key={index} className="image">
                                 <img src={image.cdn} alt="" onClick={() => setActiveImage(image)} />
@@ -91,6 +85,6 @@ export default function ImagesPage() {
             form="search"
             closeButton={true}
         />
-     
+
     </>)
 }
--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -1,5 +1,5 @@
 import type { RootState } from "./store";
-import { ErrorsTypes,  ImagesInitialStateTypes } from "libs/types";
+import { ErrorsTypes, ImageTypes, ImagesInitialStateTypes } from "libs/types";
 
 
 export const selectGlobal = (state: RootState) => state.global;
@@ -9,6 +9,10 @@ export const selectImages = (state: RootState): ImagesInitialStateTypes => state
 export const selectErrors = (state: RootState): { errors: ErrorsTypes, showMessage: boolean } => state.errors;
 
 
-
-
-
+export const selectImagesByFilters = (state: RootState): ImageTypes[] => state.images.images
+    .filter(item => item.allowed_users === 'all')
+    .filter(item => state.images.filters.category === 'all' || item.category === state.images.filters.category)
+    .filter(item => state.images.filters.query.trim().length === 0 ||
+        item.title.includes(state.images.filters.query.trim()) ||
+        item.description?.includes(state.images.filters.query.trim())
+        || item.tags.some(tag => tag.title.includes(state.images.filters.query.trim()))).sort((a, b) => state.images.filters.order === '-id' ? a.id - b.id : b.id - a.id)
