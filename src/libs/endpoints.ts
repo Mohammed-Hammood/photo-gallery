336f5cfb27bd534c14f183e61becddbcaca6319d
Mohammed <iraqiProfessionalDesigner@gmail.com>
2023-04-30 13:12:23 +0700

    changes in endponts.ts

--- src/libs/endpoints.ts
+++ src/libs/endpoints.ts
@@ -4,7 +4,6 @@ import { ImagesFiltersTypes } from "./types";
 const expressAPI = 0 ? "http://localhost:3000/api" : "https://photo-gallery-api.vercel.app/api";
 
 const images = (data: ImagesFiltersTypes): string => `${expressAPI}/images/`;
-// const images = ({ query, page, limit, category, order }: ImagesFiltersTypes): string => `${baseUrl}/images/?page=${page}&category=${category}&limit=${limit}&query=${query}&order=${order}`;
 
 export const Endpoints = {
     images,
